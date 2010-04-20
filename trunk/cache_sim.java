58d1d6b5f02dc93c50f8d16570e51cc469492770
JonKBlack <JonKBlack@9c3397cb-5c20-2ecd-f178-f775c97aeb92>
2010-04-20 01:34:29 +0000

    Added test trace file, file input through stdin works

--- trunk/cache_sim.java
+++ trunk/cache_sim.java
@@ -7,8 +7,12 @@ class cache_sim {
     private int cache_blocksize;
     private int cache_associativity;
 
+    final static int CACHE_READ = 0;
+    final static int CACHE_WRITE = 1;
+
+
     //A class used to contain cache data
-    private class cache_entry {
+    private static class cache_entry {
 	private boolean valid;
 	private boolean dirty;
 	private String[] data;
@@ -26,7 +30,7 @@ class cache_sim {
     }
 
     //A container class for cache data
-    private class set_block {
+    private static class set_block {
 	private cache_entry[] entries;
 	private ArrayList<String> LRUcontainter;
 	
@@ -37,7 +41,7 @@ class cache_sim {
 	 */
 	public set_block(int datablocks, int numEntries){
 	    entries = new cache_entry[datablocks];
-	    LRUcontainer = new ArrayList<String>(); //our LRU "queue"
+	    ArrayList<Integer> LRUcontainer = new ArrayList<Integer>(); //our LRU "queue"
 
 	    for(int i = 0; i < numEntries; i++){
 		entries[i] = new cache_entry(datablocks, "00000000", new String[datablocks]);
@@ -48,44 +52,105 @@ class cache_sim {
 
 	
     }
+    
+    private static class cache {
+	private set_block[] sets;
+	
+	public cache(int capacity, int blocksize, int associativity){
+	    //Capacity comes in as kilobytes so multiply by (1024/16) or 64 to get the capacity in blocks
+	    capacity *= 64;
+	    int numofentries = capacity / blocksize;
+	    numofentries /= associativity;
+	    sets = new set_block[associativity];
+	    for( int i = 0; i < associativity; i++){
+		sets[i] = new set_block(blocksize, numofentries);
+	    }
+
+	}
+    }
 
     //Main memory
     /*
      *  @param size: the size of memory in Megabytes * 1024^2
      */
-    private class memory {
-	private String[] data;
+    private static class memory {
+	private int[] data;
 
 	public memory(int size){
-	    data = new String[size];
+	    data = new int[size];
 	    for(int i=0; i<size; i++){
-		data[i] = Integer.toHexString(i);
+		data[i] = i;
 	    }
 	}
-	public setBlock(){
-	
+
+	public void setBlock(int address, int value){
+	    data[address] = value;
 	}
-	public getBlock(){
-	
+
+	public int getBlock(int address){
+	    return data[address];
 	}
     }
     
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException {
 	cache_sim c = new cache_sim();
 	if(!c.parseParams(args)) {
 	    return;
 	}
 	
-	io readwrite = new io();
-        
-	
-	/*
-	System.out.println("c is " + c.cache_capacity +
-			   ", b is " + c.cache_blocksize + 
-			   ", a is " + c.cache_associativity + "\n");*/
-    }
+	int i;
+        int read_write;
+        int address;
+        int data; 
 
+	memory mem = new memory(8192000); 
+ 
+	// Initialize Cache
+	cache cachemem = new cache(c.cache_capacity, c.cache_blocksize, c.cache_associativity);
+
+	String parseString;
+	String[] strings;
+	byte character;
+
+	// Read until a newline
+	while ((character = (byte)System.in.read()) != -1) {
+	    parseString = "";
+
+	    while(character != '\n') {
+		parseString += (char)character;
+		character = (byte)System.in.read();
+	    }
+
+	    strings = parseString.split("\\s");
+
+	    if(strings.length < 2) {
+		break;
+	    }
+	    // Read the first character of the line.
+	    // It determines whether to read or write to the cache.
+	    read_write = Integer.parseInt(strings[0]);
+
+	    // check again if we have reached the end
+	    // as this flag is set only after a 'cin'
+	    //	    if(feof(stdin)) return 1;
+
+
+	    // Read the address (as a hex number)
+	    address = hex_to_int(strings[1]);
+
+	    //if it is a cache write the we have to read the data
+	    if(read_write == CACHE_WRITE) {
+		
+		data = hex_to_int(strings[2]);
+		mem.setBlock(address, data);
+		
+		//output the new contents
+	    } 
+	    System.out.println("memory[" + address + "] = " + mem.getBlock(address));        
+	
+	}
+    }
 
     public boolean parseParams(String[] args)
     {
@@ -131,6 +196,29 @@ class cache_sim {
 	
 	return true;
 	
+    }
+
+    public static int hex_to_int(String s) {
+        String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
+    }
+
+    public static String int_to_hex(int input_integer) {
+	String hex_string;
+	hex_string=Integer.toHexString(input_integer);
+	while(hex_string.length()<8)
+	    {
+		hex_string = "0" + hex_string;
+	    }
+	
+	return hex_string;
     }
 
 }
