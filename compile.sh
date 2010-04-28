130d91a68c328e861ee31229a0be8433f25f5916
jonkblack <jonkblack@gmail.com>
2010-04-28 05:07:23 +0000

    CacheRead works, just need to debug CacheWrite now. Debug session and .toString() method all thats left now I think.

--- compile.sh
+++ compile.sh
@@ -2,23 +2,43 @@
 #
 
 # Build tool to compile the source and run it with given arguments
+clean=0
+compile=0
+debug=0
 
-# First check to see if there is the correct number of arguments
-# This doesn't work yet, I'll work on it later
-#if [ $# -gt 3 | $# -lt 3 ] ; then
-#    echo "THERE AREN'T THREE ARGS";
-#    exit 1
-#fi
-
-while getopts c:b:a: opt
+# First check to see if our args are correct
+while getopts c:b:a:w:m:d: opt
 do 
     case "$opt" in
         c)  capacity=$OPTARG;;
 	b)  blocksize=$OPTARG;;
 	a)  associativity=$OPTARG;;
-	\?)  echo "Usage: $0 -c -b -a"; exit 1;;
+	w)  clean=1;;
+	m)  compile=1;;
+	d)  debug=1;;
+	\?)  echo "Usage: $0 -c -b -a [-w1][-m1][-d1]"; exit 1;;
     esac
 done
 
-javac cache_sim.java
+# Clean files if we have a clean flag and don't run
+if [ $clean -eq 1 ]
+then
+    rm *.class; exit 0;
+fi
+
+# Only compile but do not run
+if [ $compile -eq 1 ]
+then
+    javac -g cache_sim.java; exit 0;
+fi
+
+# Run in debug mode
+if [ $debug -eq 1 ]
+then
+    javac -g cache_sim.java
+    jdb cache_sim.class; exit 0;
+fi
+
+#Otherwise we compile and run with the given arguments
+javac -g cache_sim.java
 java cache_sim -c$capacity -b$blocksize -a$associativity
