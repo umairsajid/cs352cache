546944084f53e0d4b7a0effc0434e5a446433da7
jonkblack@gmail.com <jonkblack@gmail.com@9c3397cb-5c20-2ecd-f178-f775c97aeb92>
2010-05-04 07:25:12 +0000

    added scrape

---
+++ trunk/scrape.py
@@ -0,0 +1,42 @@
+import re
+
+Assocs = [1, 2, 4, 8, 16]
+Blocks = [4, 8, 16,32,64,128,256,512]
+Capacity = [4,8,16,32,64]
+
+container = []
+
+for assoc in Assocs:
+    assoclist = []
+    for block in Blocks:
+        for capac in Capacity:
+            folderString = 'Assoc' + str(assoc) + 'traces/'
+            fileString = 'b' + str(block) + '-c' + str(capac) + '.out'
+            currentString = folderString + fileString
+            currentFile = open( currentString, 'r')
+            lines = currentFile.readlines()
+            #numberOmisses = lines[2]
+            if len(lines) > 2:
+                rateOmisses = lines[4]
+                searched = re.search('[0-9].[0-9]*', rateOmisses)
+                if not( searched is null ):
+                    assoclist.append(searched.groups(0))
+                else:
+                    assoclist.append(0)
+    container.append(assoclist)
+
+#Now report
+report = "Assoc. "
+for block in Blocks:
+    for capac in Capacity:
+        report += " " + str(block) + " / " + str(capacity) + " "
+print report
+
+for assoc in Assocs:
+    thisline = str(assoc) + "   "
+    i = 0
+    for block in Blocks:
+        for capac in Capacity:
+            thisline += str(container[assoc][i]) + "  "
+            i += 1
+
